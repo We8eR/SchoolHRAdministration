3ba9428d641de1334ac5a6f7e4c88b00a7872ff5
We8eR <122805348+We8eR@users.noreply.github.com>
2023-11-12 17:32:33 +0300

    Add project files.

---
+++ SchoolHRAdministration/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HRAdministrationAPI;
+
+namespace SchoolHRAdministration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    public class Teacher : EmployeeBase
+    {
+
+
+
+    }
+
+    public class HeadOfDepartment : EmployeeBase
+    { 
+        
+    
+    }
+    public class DeputyHeadMaster : EmployeeBase 
+    { 
+    
+    
+    }
+    public class HeadMaster : EmployeeBase
+    {
+
+
+    }
+}
